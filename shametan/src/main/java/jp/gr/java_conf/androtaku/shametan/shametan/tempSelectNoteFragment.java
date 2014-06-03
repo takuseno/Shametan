adde89050a8435105bbe7704d52ee5ca912109b1
takuseno <takuma.seno@gmail.com>
2014-06-03 09:48:25 +0900

    ignore

--- shametan/src/main/java/jp/gr/java_conf/androtaku/shametan/shametan/tempSelectNoteFragment.java
+++ shametan/src/main/java/jp/gr/java_conf/androtaku/shametan/shametan/tempSelectNoteFragment.java
@@ -1,15 +1,26 @@
 package jp.gr.java_conf.androtaku.shametan.shametan;
 
+/**
+ * Created by takuma on 2014/05/31.
+ */
+
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 
 import java.io.File;
@@ -19,7 +30,10 @@ import java.io.File;
  */
 public class TempSelectNoteFragment extends Fragment {
     private static final File basePath = new File(Environment.getExternalStorageDirectory().getPath() + "/Shametan/");
-    ListView listView;
+    GridView gridView;
+    FileSearch fileSearch = new FileSearch();
+
+    File[] imageFiles;
 
     public static TempSelectNoteFragment newInstance(){
         TempSelectNoteFragment fragment = new TempSelectNoteFragment();
@@ -35,21 +49,21 @@ public class TempSelectNoteFragment extends Fragment {
     }
 
     public void init(View v){
-        listView = (ListView)v.findViewById(R.id.noteList);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),android.R.layout.simple_list_item_1);
-        FileSearch fileSearch = new FileSearch();
-        fileSearch.searchFolder(basePath,".txt","aaa","aaa","aaa");
-        final File[] list = fileSearch.getFileList();
-        for(int i = 0;i < list.length;++i){
-            adapter.add(list[i].getName());
-        }
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        gridView = (GridView)v.findViewById(R.id.noteList);
+        gridView.setNumColumns(2);
+        fileSearch.searchFolder(basePath,".jpg",".JPG",".png",".PNG");
+        //ArrangeImages arrangeImages = new ArrangeImages(fileSearch.getFileList());
+        imageFiles = fileSearch.getFileList();
+        final NoteGridAdapter adapter = new NoteGridAdapter(getActivity(),R.layout.grid_items,imageFiles);
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                toNote(list[position].getPath());
+                int index = imageFiles[position].getPath().lastIndexOf(".");
+                String dataPath = imageFiles[position].getPath().substring(0, index) + ".txt";
+                toNote(dataPath);
             }
         });
-        listView.setAdapter(adapter);
     }
 
     public void toNote(String filePath){
